09c471ac6411550d0b519e0bbff3b2a70e7aff68
arezki-max <faridd.arezki@yahoo.fr>
2020-04-05 14:29:32 +0200

    premier ccommit

---
+++ src/pages/CreateComments.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Form      from 'react-bootstrap/Form';
+import Button    from 'react-bootstrap/Button';
+
+
+
+const CreateComments = () => {
+    const [ title,   setTitle   ] = useState("");
+    const [ content, setContent ] = useState("");
+    const [ prenom,  setPrenom  ] = useState("");
+    const [ author,  setAuthor  ] = useState("");
+
+
+    const handleSubmit = (event) => {
+        event.preDefault();
+        console.log("titre  :  ", title);
+        console.log("content:  ", content);
+        console.log("prenom :  ", prenom);
+        console.log("author :  ", author);
+
+    }
+
+    const handleChange = (event) => {
+        console.log("target name  : ", event.target.name);
+        console.log("target value : ", event.target.value);
+
+        switch(event.target.name) {
+            case "title":
+                setTitle        (event.target.value);
+                break;
+            case "content":
+                setContent      (event.target.value);
+                break;
+            case "prenom":
+                setPrenom       (event.target.value);
+                break;
+            case "author":
+                setAuthor       (event.target.value);
+
+            // no default
+        }
+
+    }
+    return (
+        <Container>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="comment.title">
+                    <Form.Label>Titre du commentaires</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="title"
+                        onChange={handleChange}
+                        value={title}
+                    />
+                </Form.Group>
+                <Form.Group controlId="comment.content">
+                    <Form.Label> Contenu du commentaire </Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        name="content"
+                        onChange={handleChange}
+                        value={content}
+                    />
+                </Form.Group>
+                <Form.Group controlId="comment.prenom">
+                    <Form.Label> prenom </Form.Label>
+                    <Form.Control
+                        type="textarea"
+                        name="prenom"
+                        onChange={handleChange}
+                        value={prenom}
+                    />
+                </Form.Group>
+                <Form.Group controlId="comment.author">
+                    <Form.Label>id de l'auteur</Form.Label>
+                    <Form.Control
+                        type="number"
+                        name="author"
+                        onChange={handleChange}
+                        value={author}
+                    />
+                </Form.Group>
+
+                <Button variant="danger" type="submit">Créer le commentaire</Button>
+            </Form>
+
+        </Container>
+
+    )
+
+};
+
+export default CreateComments;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Badge     from 'react-bootstrap/Badge';
+const Home = () => {
+    return (
+        <div>
+
+            <h1>Page d accueil</h1>
+
+           <Badge variant="success"> site de farid</Badge>
+     </div>
+    );
+};
+
+export default Home;
